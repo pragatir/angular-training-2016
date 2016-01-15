0959cc656dd12bda08e2a4645ed2c62a5df06fd9
Pragati Rithekar <pragati.rithekar@globant.com>
2016-01-15 15:45:12 +0530

    Made changes to the form of TODO

--- todoApp/app/scripts/controllers/main.js
+++ todoApp/app/scripts/controllers/main.js
@@ -9,10 +9,29 @@
  */
 angular.module('todoAppApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.todos = [{
+      "title" : "Get Susan a birthday gift",
+      "description" : "Has a birthday on 20th of this month. She likes flowers.",
+      "priority" : "High"
+    }, {
+      "title" : "Call hotel to check on rooms",
+      "description" : "Need 2 double occupancy rooms",
+      "priority" : "Medium"
+    }, {
+      "title" : "Send invoice to Carl for Phase 2",
+      "description" : "Carl email : carl@company.com",
+      "priority" : "Low"
+    }];
     $scope.addTodo = function () {
-      $scope.todos.push($scope.todo);
-      $scope.todo = '';
+      var todo = {
+        "title" : $scope.title,
+        "description" : $scope.description,
+        "priority" : $scope.priority
+      }
+      $scope.todos.push(todo);
+      $scope.title = '';
+      $scope.description = '';
+      $scope.priority = '';
     };
     $scope.removeTodo = function (index) {
       $scope.todos.splice(index, 1);
