e99cf29171d29e4c5cc4e55dd63367668f844582
Pragati Rithekar <pragati.rithekar@globant.com>
2016-01-29 20:49:31 +0530

    Added anchorScroll example

--- basic-examples/$anchorScroll/app.js
+++ basic-examples/$anchorScroll/app.js
@@ -1,7 +1,16 @@
 (function () {
 	var app = angular.module("anchorScrollSample", []);
 
-	app.controller("anchorScrollCtrl", ["$scope", "$location", function ($scope, $location) {
-		
+	app.controller("anchorScrollCtrl", ["$scope", "$location", "$anchorScroll", function ($scope, $location, $anchorScroll) {
+		//$anchorScroll.yOffset = 10;
+		$scope.jumpToContainer3 = function () {
+			$location.hash("three");
+			$anchorScroll();
+		}
+
+		$scope.gotoSpecified() = function (id) {
+			$location.hash(id);
+			$anchorScroll();
+		}
 	}]);
 } ());
